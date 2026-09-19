30c53bc5f8acc500b7ab4e74ba64cc35a13a6c71
VXNUS <github@vxnus.xyz>
2026-09-19 16:08:01 +0700

    chore(release): bump @vxnus/siduri to 2.0.39; sync generator and docs

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -13,7 +13,7 @@ import { runDbPush } from './db';
 import { configureOrgan, OrganConfigurationResult } from './configurators';
 
 const execFile = promisify(execFileCallback);
-export const CLI_VERSION = '2.0.38';
+export const CLI_VERSION = '2.0.39';
 
 import { colors } from './colors';
 export { colors };
